a5718658bd0804ba00e6fe4e7456ccbd3304f5ba
Jason Tran <jjsontran@gmail.com>
2023-05-14 15:23:50 -0400

    added homepage components

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,13 +2,41 @@ import React from "react";
 import styled from "styled-components";
 
 const HomeContainer = styled.div`
-  background-color: #ffb668;
   width: 100vw;
-  height: 100vh;
+  height: fit-content;
+
+  display: flex;
+  flex-direction: column;
+
+  background-color: #fafafa;
+`;
+
+const SectionWrapper = styled.div``;
+
+const SON = styled.img`
+  max-width: 100vw;
+  max-height: 90vh;
+  width: fit-content;
+  float: right;
+`;
+const NAE = styled.img`
+  max-width: 100vw;
+  max-height: 90vh;
+  width: fit-content;
+  margin-top: 10vh;
 `;
 
 const Home = () => {
-  return <HomeContainer id="Home">Home</HomeContainer>;
+  return (
+    <HomeContainer id="Home">
+      <div>
+        <SON src="./assets/SON.png" />
+      </div>
+      <SectionWrapper>
+        <NAE src="./assets/NAE.png" />
+      </SectionWrapper>
+    </HomeContainer>
+  );
 };
 
 export default Home;
